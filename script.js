eb3cb3758beae31ef2d90419d32d17d24c58c128
Game_master19032 <Game_master19032@outlook.fr>
2026-01-22 13:18:34 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const ProjetBasculement = document.querySelector('.ProjetBasculement');
+const Recto = document.querySelector('.ProjetRecto');
+const Verso = document.querySelector('.ProjetVerso');
+
+ProjetBasculement.onclick = function() {
+    if (ProjetBasculement.textContent === '>') {
+        ProjetBasculement.textContent = '<';
+        Recto.style.display = 'inline';
+        Verso.style.display = 'inline';
+    }
+    else if (ProjetBasculement.textContent === '<') {
+        ProjetBasculement.textContent = '>';
+        Recto.style.display = 'none';
+        Verso.style.display = 'none';
+    }
+};
+const Logo = document.querySelector('.Logo');
+Logo.onclick = function() {
+    window.location.href = 'index.html';
+}
